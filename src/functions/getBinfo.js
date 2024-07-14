2bc75ec7be292544cf0d30153f28735a453ec2eb
Andrew Chart <achart@hotmail.com>
2024-07-14 23:27:56 +0100

    Hello binfo

--- src/functions/getBinfo.js
+++ src/functions/getBinfo.js
@@ -3,6 +3,6 @@ const { app } = require('@azure/functions');
 app.timer('getBinfo', {
     schedule: '0 0 1 * * 1',
     handler: (myTimer, context) => {
-        context.log('Timer function processed request.');
+        context.log('Hello Binfo.');
     }
 });
