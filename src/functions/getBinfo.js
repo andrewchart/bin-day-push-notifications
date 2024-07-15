43bd058b80f2d48f58717de649ee5700a6e23329
Andrew Chart <achart@hotmail.com>
2024-07-15 23:46:50 +0100

    Implements browser automation to reach bin collection information page

--- src/functions/getBinfo.js
+++ src/functions/getBinfo.js
@@ -1,8 +1,52 @@
 const { app } = require('@azure/functions');
+const puppeteer = require("puppeteer");
 
 app.timer('getBinfo', {
     schedule: '0 0 1 * * 1',
-    handler: (myTimer, context) => {
+    handler: async function(myTimer, context) {
+
         context.log('Hello Binfo.');
+
+        const { 
+            START_URL,
+            FORM_PAGE_LINK_TEXT,
+            PROPERTY_NAME_OR_NUMBER,
+            STREET,
+            POSTCODE 
+        } = process.env;
+
+        const browser = await puppeteer.launch({ headless: false });
+        const page = await browser.newPage();
+
+        // Go to start page
+        await page.goto(START_URL);
+
+        // Navigate to address form
+        await Promise.all([
+            page.locator(`a ::-p-text(${FORM_PAGE_LINK_TEXT})`).click(),
+            page.waitForNavigation
+        ]);
+
+        // Fill in the form
+        await page.locator('#address_name_number').fill(PROPERTY_NAME_OR_NUMBER);
+        await page.locator('#address_street').fill(STREET);
+        await page.locator('#address_postcode').fill(POSTCODE);
+
+        // Submit form
+        await Promise.all([
+            page.locator('#Submit').click(),
+            page.waitForNavigation
+        ]);
+
+        // Choose first address
+        await Promise.all([
+            page.locator('#property_list a').click(),
+            page.waitForNavigation
+        ]);
+
+        // TODO: Parse data
+
+        await browser.close();
+    
     }
 });
