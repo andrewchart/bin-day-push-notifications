240362b817cb084b92556e6f07ab1fd94074b89d
Andrew Chart <achart@hotmail.com>
2024-08-04 23:37:06 +0100

    Try without additional function in index.js

--- src/index.js
+++ src/index.js
@@ -1,9 +1,5 @@
 const { app } = require('@azure/functions');
-const deployStaticContent = require('./site/deployStaticContent.js');
 
 app.setup({
     enableHttpStream: true,
 });
-
-// Deploys the webpage where users can subscribe to notifications
-deployStaticContent();
