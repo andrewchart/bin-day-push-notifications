b4866f37f83d092def390d8e117bf5b7951e099d
Andrew Chart <achart@hotmail.com>
2024-11-17 13:07:58 +0000

    Changes schedule for test

--- src/functions/sendNotifications.js
+++ src/functions/sendNotifications.js
@@ -4,7 +4,7 @@ const { NotificationHubsClient, createBrowserNotification } = require('@azure/no
 
 
 app.timer('sendNotifications', {
-    schedule: '0 */6 11 * * *',
+    schedule: '0 */6 13 * * *',
     handler: async function(myTimer, context) {
 
         let now = new Date();
